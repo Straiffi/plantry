244331fecca33490682f3d175c564b1179342e31
Juuso <jsoikkeli90@gmail.com>
2026-05-02 15:40:30 +0300

    add vercel route handler for api

---
+++ api/[...route].ts
@@ -0,0 +1,12 @@
+import { handle } from 'hono/vercel'
+
+import { app } from './src/app.js'
+
+const handler = handle(app)
+
+export const DELETE = handler
+export const GET = handler
+export const OPTIONS = handler
+export const PATCH = handler
+export const POST = handler
+export const PUT = handler
